02838dbeb0f11a3e18d9d3020b7f2106d558751d
Mel Maki <melody.rackham@gmail.com>
2019-12-13 12:08:09 -0700

    MVP

--- data/seeds/002-tasks.js
+++ data/seeds/002-tasks.js
@@ -2,19 +2,19 @@ exports.seed = function(knex) {
   return knex('tasks').insert([
     {
       project_id: '1',
-      description: 'clean',
+      task_description: 'clean',
       notes: 'Make sure you clean the toilets please',
       completed: 0,
     },
     {
       project_id: '2',
-      description: 'Couch',
+      task_description: 'Couch',
       notes: 'The couch is the most comfortable place to watch Star Wars',
       completed: 0,
     },
     {
       project_id: '3',
-      description: 'Sewing',
+      task_description: 'Sewing',
       notes: 'She wants it to be 8 ft by 8 ft',
       completed: 0,
     },
